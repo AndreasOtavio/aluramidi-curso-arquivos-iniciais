59f3cd86ce7dd864fda147c798a50f23430dc727
Otavio <otaviorosiano@gmail.com>
2023-08-21 22:50:19 -0300

    Primeiro commit - basic drum

---
+++ main.js
@@ -0,0 +1,25 @@
+function tocaSom(idElementoAudio){
+
+    document.querySelector(idElementoAudio).play();
+}
+
+const listadeTeclas = document.querySelectorAll('.tecla');
+
+
+for (let contador = 0; contador < listadeTeclas.length; contador++){    
+
+    const tecla = listadeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    //para este tipo, seria necessário colocar no parametro da função abaixo a idAudio
+    //dessa forma atribuindo o #som_${instrumento} com crase ao invés de aspas duplas
+    //template string
+    //const idAudio = `#som_${instrumento}`;
+
+    console.log(instrumento);
+
+
+    tecla.onclick = function(){
+        tocaSom('#som_'+instrumento);
+    }
+}
